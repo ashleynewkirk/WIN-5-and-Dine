4eb0e498a6d1c0f22ce25aa44c1c579c04cffc8b
eduardog3000 <eduardog3000@gmail.com>
2019-04-30 10:37:25 -0400

    Initial commit

---
+++ src/components/Restaurant.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../css/Restaurant.css';
+
+class Restaurant extends React.Component {
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
